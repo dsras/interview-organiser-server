40bdee6c573d7a1fa46d3af5167eed3c174780ec
emersweeney <emer.sweeney@accolitedigital.com>
2022-03-28 13:31:44 +0100

    Spring Boot App foundation

---
+++ src/test/java/com/au/glasgow/InterviewOrganiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.au.glasgow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewOrganiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
